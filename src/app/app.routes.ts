3b00b6dddf614aae0fc04a2389e41492170a069f
Kapil Kandel <kapil@portpro.io>
2024-05-28 17:02:11 +0545

    route updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'login', component: LoginComponent, title: 'Login' },
     { path: 'signup', component: SignupComponent, title: 'Sign Up' },
+    { path: 'forgot-password', component: ForgotPasswordComponent, title: 'Forgot Password' },
     { path: '**', redirectTo: '' } // Wildcard route for a 404 page
 ];
 

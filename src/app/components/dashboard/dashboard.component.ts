19c96eb101242572cd1f5c060d4000fdbc0cda9c
Kapil Kandel <kapil@portpro.io>
2024-05-28 17:01:45 +0545

    dashboard changes

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NAVBAR_ITEMS } from './navbarItems'
+import { BarChartComponent } from '../bar-chart/bar-chart.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule,BarChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  selectedButton: string = NAVBAR_ITEMS.Dashboard;
+  navbarItems: string [] = Object?.values(NAVBAR_ITEMS);
+
+  setSelected(button: string) {
+    this.selectedButton = button;
+  }
 
 }
